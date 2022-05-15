5fab0578ccab36de913e45d607b7e203622f35c5
VolodymyrNehir <volodymyrnehir@gmail.com>
2022-05-16 01:25:14 +0300

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {PostsComponent} from './components/posts/posts.component';
+import {CommentsComponent} from './components/comments/comments.component';
+import {PostComponent} from './components/post/post.component';
+import {CommentComponent} from './components/comment/comment.component';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    CommentsComponent,
+    PostComponent,
+    CommentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'posts', component: PostsComponent },
+      { path: 'comments', component: CommentsComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
