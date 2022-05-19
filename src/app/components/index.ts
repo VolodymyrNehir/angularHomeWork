0b273ea4c8d9a40cefa522c849f3b439e7a7c8b1
VolodymyrNehir <volodymyrnehir@gmail.com>
2022-05-19 23:46:18 +0300

    add

--- src/app/components/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./comment/comment.component";
-export * from "./post/post.component";
-export * from "./posts/posts.component";
-export * from "./comments/comments.component";
-export * from "./post-details/post-details.component";
-export * from "./comment-details/comment-details.component";
