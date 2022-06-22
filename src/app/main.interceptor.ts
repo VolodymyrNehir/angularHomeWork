8870b7876bfa181c4f829fef1e9d32086689aebe
VolodymyrNehir <volodymyrnehir@gmail.com>
2022-06-23 00:16:33 +0300

    add

--- src/app/main.interceptor.ts
+++ src/app/main.interceptor.ts
@@ -12,10 +12,14 @@ import {Router} from "@angular/router";
 @Injectable()
 export class MainInterceptor implements HttpInterceptor {
 
-  constructor(authService:AuthService, router:Router) {}
+  constructor(private authService:AuthService, private router:Router) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return next.handle(request);
+const authorization = this.authService.isAuthorization();
+
+if (authorization){
+}
 
   }
 
