1488e99a59814eba0d86afccb662c052fd8394bb
VolodymyrNehir <volodymyrnehir@gmail.com>
2022-07-12 00:30:26 +0300

    add

--- src/app/components/cars/cars.component.ts
+++ src/app/components/cars/cars.component.ts
@@ -1,10 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {ICar} from "../../interface";
-import {CarsService} from "../../services";
-import {RegEx} from '../../contents/index';
 
-
+import {ICar} from '../../interfaces';
+import {CarService} from '../../services';
+import {RegEx} from '../../contants';
 
 @Component({
   selector: 'app-cars',
@@ -12,45 +11,52 @@ import {RegEx} from '../../contents/index';
   styleUrls: ['./cars.component.css']
 })
 export class CarsComponent implements OnInit {
-  cars:ICar[];
+  cars: ICar[];
   form: FormGroup;
   carForUpdate: ICar | null;
 
-
-  constructor(private carsService:CarsService) {
-    this.createForm()
+  constructor(private carService: CarService) {
+    this._creteForm()
   }
 
   ngOnInit(): void {
-    this.carsService.getAll().subscribe(cars => console.log(cars) )
+    this.carService.getAll().subscribe(value => this.cars = value)
 
   }
 
-  save() {
+  save(): void {
     if (!this.carForUpdate) {
-      this.carsService.create(this.form.value).subscribe(value => {
+      this.carService.create(this.form.value).subscribe(value => {
         this.cars.push(value)
         this.form.reset()
       })
     } else {
-      this.carsService.updateById(this.carForUpdate.id, this.form.value).subscribe(value => {
+      this.carService.updateById(this.carForUpdate.id, this.form.value).subscribe(value => {
         const updateCar = this.cars.find(f => f.id === this.carForUpdate?.id);
         Object.assign(updateCar, value)
+        console.log(this.carForUpdate)
         this.carForUpdate = null
       })
     }
   }
 
-  createForm(): void{
-    this.form = new FormGroup({
-      model: new FormControl(null, [Validators.pattern(RegEx.model)]),
-      year: new FormControl(1990,[Validators.min(1990), Validators.max(new Date().getFullYear())]),
-      price: new FormControl(0,[Validators.min(0)])
+  delete(id: number): void {
+    this.carService.deleteById(id).subscribe(() => {
+      const index = this.cars.findIndex(car => car.id === id);
+      this.cars.splice(index, 1)
     })
   }
 
-  update(cars: ICar): void {
-    this.carForUpdate = cars
-    this.form.setValue({model: cars.model, year: cars.year, price: cars.price})
+  _creteForm(): void {
+    this.form = new FormGroup({
+      model: new FormControl(null, [Validators.pattern(RegEx.model)]),
+      year: new FormControl(1990, [Validators.min(1990), Validators.max(new Date().getFullYear())]),
+      price: new FormControl(0, [Validators.min(0), Validators.max(1000000)])
+    })
+  }
+
+  update(car: ICar): void {
+    this.carForUpdate = car
+    this.form.setValue({model: car.model, year: car.year, price: car.price})
   }
 }
